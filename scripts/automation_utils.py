807b1d22039611116618b1ad523b3cdd88f00bde
jmc07712 <jmc07712@yahoo.com>
2020-04-22 23:19:02 -0400

    Fixed default path typo

--- scripts/automation_utils.py
+++ scripts/automation_utils.py
@@ -6,7 +6,7 @@ from stat import S_IEXEC
 
 doneWorking = False
 
-DEFAULT_FLIP_PATH = '/moseq2_data/flip_files/flip_classifier/flip_classifier_k2_c57_10to13weeks.pkl'
+DEFAULT_FLIP_PATH = '/moseq2_data/flip_files/flip_classifier_k2_c57_10to13weeks.pkl'
 BATCH_TABLE = {'extract-batch': ['--input-dir', '-i', '--config-file', '-c', '--filename']}
 EXTRACT_TABLE = {'generate-config': ['-o', '--output-file'],
                 'extract': ['--config-file', '--flip-classifier']}
